ef03cf13c200db80557ef4738c23e5e52ad778c4
GeraldAkenji <akenjigerald@gmail.com>
2024-07-06 00:01:05 -0600

    initial commit

---
+++ providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "BertillaTifuh"
+  # Configuration options
+}
